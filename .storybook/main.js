86cb8dfe27732d57b2e8111371b5dedaf7043025
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-03-10 20:23:28 -0800

    feat: automatic split bundling

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  features: {
+    storyStoreV7: true, // automatic split bundling
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
